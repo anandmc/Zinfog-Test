0b3a2610bf7b29e4bcf73fc68faeebf8f02d11f4
anandmc <mcanand1698@gmail.com>
2024-07-31 22:36:54 +0530

    [ADD][MOD][sale_deliveery]

---
+++ sale_stock_multiple_delivery/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Sale Multiple Delivery Orders",
+    'depends': ['sale_stock'],
+    'description': "waiters, devices, cashiers, full access employees",
+    'author': 'ANAND MC',
+    'data': [
+
+    ],
+    'installable': True,
+    'assets': {
+
+    },
+    'license': 'LGPL-3',
+}
---
+++ sale_stock_multiple_delivery/models/sale_order.py
@@ -0,0 +1,93 @@
+from odoo import models, fields
+from odoo.tools import float_compare
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # TODO if need to change creation of multiple delivery
+    # by product add in sale order views
+    multiple_delivery_order = fields.Boolean(default=True)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
+        """
+        Launch procurement group run method with required/custom fields genrated by a
+        sale order line. procurement group will launch '_run_pull', '_run_buy' or '_run_manufacture'
+        depending on the sale order line product rule.
+
+        ######## -- replaced function to create multiple delivery orders as per products -- ########
+        """
+        if self._context.get("skip_procurement"):
+            return True
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        procurements = []
+        for line in self:
+            line = line.with_company(line.company_id)
+            if line.state != 'sale' or not line.product_id.type in ('consu', 'product'):
+                continue
+            qty = line._get_qty_procurement(previous_product_uom_qty)
+            if float_compare(qty, line.product_uom_qty, precision_digits=precision) == 0:
+                continue
+
+            group_id = line._get_procurement_group()
+
+            ################## if the boolean is true then make the move not identical(create new group)
+            if line.order_id.multiple_delivery_order:
+                group_id = self.env['procurement.group'].create(line._prepare_procurement_group_vals())
+                line.order_id.procurement_group_id = group_id
+            #############
+            if not group_id:
+                group_id = self.env['procurement.group'].create(line._prepare_procurement_group_vals())
+                line.order_id.procurement_group_id = group_id
+            else:
+                # In case the procurement group is already created and the order was
+                # cancelled, we need to update certain values of the group.
+                updated_vals = {}
+                if group_id.partner_id != line.order_id.partner_shipping_id:
+                    updated_vals.update({'partner_id': line.order_id.partner_shipping_id.id})
+                if group_id.move_type != line.order_id.picking_policy:
+                    updated_vals.update({'move_type': line.order_id.picking_policy})
+                if updated_vals:
+                    group_id.write(updated_vals)
+
+            values = line._prepare_procurement_values(group_id=group_id)
+            product_qty = line.product_uom_qty - qty
+
+            line_uom = line.product_uom
+            quant_uom = line.product_id.uom_id
+            product_qty, procurement_uom = line_uom._adjust_uom_quantities(product_qty, quant_uom)
+            ########## check product allready exist in procurement if exist
+            ########## delete the tupple and create new one with product qty
+            same_prod_proc = [x for x in procurements if line.product_id.id == x.product_id.id]
+            if same_prod_proc and line.order_id.multiple_delivery_order:
+                product_qty = sum(
+                    [x.product_qty for x in procurements if line.product_id.id == x.product_id.id]) + product_qty
+                procurements.remove(same_prod_proc[0])
+                procurements.append(self.env['procurement.group'].Procurement(
+                    line.product_id, product_qty, procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.product_id.display_name, line.order_id.name, line.order_id.company_id, values))
+            else:
+                procurements.append(self.env['procurement.group'].Procurement(
+                    line.product_id, product_qty, procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.product_id.display_name, line.order_id.name, line.order_id.company_id, values))
+            ######################
+        if procurements:
+            procurement_group = self.env['procurement.group']
+            if self.env.context.get('import_file'):
+                procurement_group = procurement_group.with_context(import_file=False)
+            procurement_group.run(procurements)
+
+        # This next block is currently needed only because the scheduler trigger is done by picking confirmation rather than stock.move confirmation
+        orders = self.mapped('order_id')
+        for order in orders:
+            pickings_to_confirm = order.picking_ids.filtered(lambda p: p.state not in ['cancel', 'done'])
+            if pickings_to_confirm:
+                # Trigger the Scheduler for Pickings
+                pickings_to_confirm.action_confirm()
+        return True
